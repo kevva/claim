6a9d0e18c4d479e03aa932d7cfa93d9b10faa4c6
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-09 15:12:31 +0700

    improve test titles

--- test.js
+++ test.js
@@ -46,39 +46,39 @@ test('.notOk()', function (t) {
 	t.end();
 });
 
-test('should be true', function (t) {
+test('.true()', function (t) {
 	claim.true(true);
 	t.end();
 });
 
-test('should be false', function (t) {
+test('.false()', function (t) {
 	claim.false(false);
 	t.end();
 });
 
-test('should be equal', function (t) {
+test('.is()', function (t) {
 	claim.is('foo', 'foo');
 	t.end();
 });
 
-test('should not be equal', function (t) {
+test('.not()', function (t) {
 	claim.not('foo', 'bar');
 	t.end();
 });
 
-test('should be same', function (t) {
+test('.same()', function (t) {
 	claim.same({a: 'a'}, {a: 'a'});
 	claim.same(['a', 'b'], ['a', 'b']);
 	t.end();
 });
 
-test('should not be same', function (t) {
+test('.notSame()', function (t) {
 	claim.notSame({a: 'a'}, {a: 'b'});
 	claim.notSame(['a', 'b'], ['c', 'd']);
 	t.end();
 });
 
-test('should throw an error', function (t) {
+test('.throws()', function (t) {
 	claim.throws(function () {
 		claim.is('foo', 'bar');
 	});
@@ -86,7 +86,7 @@ test('should throw an error', function (t) {
 	t.end();
 });
 
-test('should not throw an error', function (t) {
+test('.doesNotThrow()', function (t) {
 	claim.doesNotThrow(function () {
 		claim.is('foo', 'foo');
 	});
@@ -94,22 +94,15 @@ test('should not throw an error', function (t) {
 	t.end();
 });
 
-test('should not be an error', function (t) {
-	claim.error(false);
-	t.end();
-});
-
-test('should match a regex', function (t) {
+test('.regexTest()', function (t) {
 	claim.regexTest(/^abc$/, 'abc');
 	t.end();
 });
 
-test('should be an error', function (t) {
-	try {
+test('.ifError()', function (t) {
+	claim.throws(function () {
 		claim.ifError(new Error());
-	} catch (err) {
-		t.assert(err);
-	}
+	});
 
 	t.end();
 });
